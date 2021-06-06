4086e81a6c3856dc0da35d1f6746ebe598acdd1b
Michael Obeng <michaelsquireobeng@gmail.com>
2021-06-06 19:52:01 +0000

    dynamic menu page

---
+++ resources/views/menu/single-menu.blade.php
@@ -0,0 +1,91 @@
+@extends('layouts.app')
+
+
+@section('content')
+  <div id="single-menu-page">
+  <div class="row">
+    <div class="col-md-10 offset-md-1">
+        <div class="content-box">
+            <div class="row">
+            <div class="col-md-12">
+                <h1>Rice Dishes</h1>
+            </div>
+                <div class="col-md-6">
+                    <div class="item">
+                        <div class=title>
+                            <h4>Waakye</h4>
+                            <span class="price">$10</span>
+                        </div>
+                        <div class="description">
+                            <p>Rice, Black Eyed Peas, Gari, Shito, 2 Hard-boiled Eggs</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="item">
+                        <div class=title>
+                            <h4>Waakye</h4>
+                            <span class="price">$10</span>
+                        </div>
+                        <div class="description">
+                            <p>Rice, Black Eyed Peas, Gari, Shito, 2 Hard-boiled Eggs</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="item">
+                        <div class=title>
+                            <h4>Waakye</h4>
+                            <span class="price">$10</span>
+                        </div>
+                        <div class="description">
+                            <p>Rice, Black Eyed Peas, Gari, Shito, 2 Hard-boiled Eggs</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="item">
+                        <div class=title>
+                            <h4>Waakye</h4>
+                            <span class="price">$10</span>
+                        </div>
+                        <div class="description">
+                            <p>Rice, Black Eyed Peas, Gari, Shito, 2 Hard-boiled Eggs</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="item">
+                        <div class=title>
+                            <h4>Waakye</h4>
+                            <span class="price">$10</span>
+                        </div>
+                        <div class="description">
+                            <p>Rice, Black Eyed Peas, Gari, Shito, 2 Hard-boiled Eggs</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="item">
+                        <div class=title>
+                            <h4>Waakye</h4>
+                            <span class="price">$10</span>
+                        </div>
+                        <div class="description">
+                            <p>Rice, Black Eyed Peas, Gari, Shito, 2 Hard-boiled Eggs</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                
+                </div>
+                <div class="col-md-6">
+                
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+    
+  </div>
+@stop
